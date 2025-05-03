4d5c8f57fbe7aa790f27859f4d81eb8384c0a038
Dave in Tucson <dave.in.tucson.az@gmail.com>
2025-05-03 15:59:37 -0700

    bug fix, exclude void transactions

--- src/sql/procedures/select-group-account-balances.sql
+++ src/sql/procedures/select-group-account-balances.sql
@@ -9,7 +9,7 @@ create procedure select_group_account_balances(
 begin
     select agr.account_group_id group_id, agr.account_id, ifnull(sum(if(agr.account_id=to_id,amount,-amount)), 0) balance 
     from account_group_rollup agr 
-    left join (select * from transactions where date_posted < p_max_date) t on agr.account_id in (to_id, from_id) 
+    left join (select * from transactions where status!='void' date_posted < p_max_date) t on agr.account_id in (to_id, from_id) 
     where account_group_id=p_group_id
     group by agr.account_group_id, agr.account_id;
 end $$
--- src/sql/procedures/select-group-summary-balances.sql
+++ src/sql/procedures/select-group-summary-balances.sql
@@ -8,12 +8,14 @@ create procedure select_group_summary_balances(
     p_to_date date
 )
 begin
-    select agr.account_group_id group_id, agr.account_id, 
-    ifnull(sum(if(date_posted < p_from_date,
-        if(agr.account_id=to_id,amount,-amount), 0)), 0) from_balance,
-    ifnull(sum(if(agr.account_id=to_id,amount,-amount)), 0) to_balance 
+    select 
+        agr.account_group_id group_id, 
+        agr.account_id, 
+        ifnull(sum(if(date_posted < p_from_date,
+                   if(agr.account_id=to_id,amount,-amount), 0)), 0) from_balance,
+        ifnull(sum(if(agr.account_id=to_id,amount,-amount)), 0) to_balance 
     from account_group_rollup agr 
-    left join (select * from transactions where date_posted <= p_to_date) t on agr.account_id in (to_id, from_id) 
+        left join (select * from transactions where status!='void' and date_posted <= p_to_date) t on agr.account_id in (to_id, from_id) 
     where account_group_id=p_group_id
     group by agr.account_group_id, agr.account_id;
 end $$
